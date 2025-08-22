3c0bef4dde65568fa0b1fa4fd4970dde25842e98
zhenkai.sun <zhenkai.sun@qq.com>
2025-08-22 23:41:49 +0000

    uypdate

--- tianchi/learn ml/531842 - News Recommend/data.py
+++ tianchi/learn ml/531842 - News Recommend/data.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 class DataManager:
     def __init__(self, base_path: str):
@@ -7,11 +8,24 @@ class DataManager:
         self.article_emb_csv_path = f'{base_path}/articles_emb.csv'
         self.train_click_csv_path = f'{base_path}/train_click_log.csv'
         self.test_click_csv_path = f'{base_path}/testA_click_log.csv'
+        # pandas
+        self.df_article = pd.read_csv(self.article_csv_path)
+        self.df_article_emb = pd.read_csv(self.article_emb_csv_path)
+        self.df_train_click = pd.read_csv(self.train_click_csv_path)
+        self.df_test_click = pd.read_csv(self.test_click_csv_path)
 
-    def describe():
-        pass
+    def describe(self):
+        print('----- article -----')
+        print(self.df_article.describe())
+        print('\n----- article emb -----')
+        print(self.df_article_emb.describe())
+        print('\n----- train click -----')
+        print(self.df_train_click.describe())
+        print('\n----- test click -----')
+        print(self.df_test_click.describe())
 
 
 if __name__ == '__main__':
-    dm = DataManager()
+    dm = DataManager('~/dataset/tianchi-news-rec')
+    dm.describe()
     print('run main done')
